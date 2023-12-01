70f3a76c02d9c5beacea404173776484e3adc920
Jean Vargas <carlosvp0324@gmail.com>
2023-12-01 00:33:09 -0500

    refactoring

--- app/services.py
+++ app/services.py
@@ -1,5 +1,5 @@
 import easyocr as ocr
 
 def get_ocr_model():
-    reader = ocr.Reader(['en'], model_storage_directory='.')
+    reader = ocr.Reader(['en'], model_storage_directory='.', gpu=False)
     return reader
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import reader
+from PIL import Image
+from io import BytesIO
 from typing import Union
 
 app = FastAPI()
@@ -15,11 +17,39 @@ app.add_middleware(
 )
 
 
+def resize_image(image_bytes, target_size=(800, 1200)):
+    # Abrir la imagen y convertirla a modo RGB
+    img = Image.open(BytesIO(image_bytes)).convert("RGB")
+
+    print(img.size)
+    # Redimensionar la imagen
+    img = img.resize(target_size)
+
+    # Crear un objeto BytesIO para almacenar la imagen redimensionada
+    buffered = BytesIO()
+
+    # Guardar la imagen redimensionada en formato JPEG
+    img.save(buffered, format="JPEG")
+
+    # Obtener los bytes de la imagen redimensionada
+    resized_image = buffered.getvalue()
+
+    return resized_image
+
 @app.post("/process_image")
 async def process_image(file: Union[UploadFile, None]):
     try:
-        result = reader.readtext( file.file.read(), paragraph=True)
-        return [item[1].upper() for item in result]
+        if file is not None:
+            # Redimensionar la imagen antes de realizar OCR
+            resized_image = resize_image(file.file.read())
+
+            # Realizar OCR en la imagen redimensionada
+            result = reader.readtext(resized_image, paragraph=True)
+
+            # Convertir a mayúsculas el segundo elemento de cada tupla en result
+            processed_result = [item[1].upper() for item in result]
+
+            return processed_result
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
